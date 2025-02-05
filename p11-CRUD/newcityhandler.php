df49a2d4252f7bb3ddb91cca4c32cfbbb998e8bf
António Cunha <tojo98@live.com.pt>
2025-02-05 19:42:32 +0000

    create p12 folder

--- p11-CRUD/newcityhandler.php
+++ p11-CRUD/newcityhandler.php
@@ -1,4 +1,7 @@
 <?php
+
+// 1.Inclusão do arquivo de conexão
+
 require_once("./db_connection.php");
 
 // $name = isset($_POST["name"]) ? $_POST["name"] : false;
@@ -6,15 +9,21 @@ require_once("./db_connection.php");
 // $district = isset($_POST["district"]) ? $_POST["district"] : false;
 // $population = isset($_POST["population"]) ? $_POST["population"] : false;
 
+// 2. Verificação e obtenção de dados do formulário
+
 if (isset($_POST["name"])) {
     $name = $_POST["name"];
     $countryCode = $_POST["countryCode"];
     $district = $_POST["district"];
     $population = $_POST["population"];
 
+    // 3. Inserção no banco de dados com prepared statements
+
     $sql = "INSERT INTO city VALUES(NULL, ?, ?, ?, ?)";
     $statement = mysqli_prepare($mysqli, $sql);
     mysqli_stmt_bind_param($statement, "sssi", $name, $countryCode, $district, $population);
+
+    // 4. Execução e redirecionamento
 
     $result = mysqli_stmt_execute($statement);
 
@@ -23,6 +32,9 @@ if (isset($_POST["name"])) {
     } else {
         die("Error ao inserir");
     }
+
+    // 5. Tratamento de erro
+
 } else {
     die("Error ao inserir");
 }
